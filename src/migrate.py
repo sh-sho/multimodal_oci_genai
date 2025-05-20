e91bf5c4437b7cc28e428a6830615312a10db7a3
sh-sho <sho.obata.c5@gmail.com>
2025-05-20 02:44:52 +0000

    fix: utils

--- src/migrate.py
+++ src/migrate.py
@@ -1,49 +1,14 @@
 import os
-import oci
-from oci.generative_ai_inference.models import (
-    ChatDetails, 
-    CohereChatRequest, 
-    OnDemandServingMode, 
-    CohereTool, 
-    CohereParameterDefinition,
-    LlamaLlmInferenceRequest,
-    GenericChatRequest,
-    TextContent,
-    ImageContent,
-    ImageUrl,
-    Message,
-    ChatContent,
-    BaseChatRequest
-)
-from oci.generative_ai_inference import GenerativeAiInferenceClient
-from oci.object_storage import ObjectStorageClient
 import oracledb
-import requests
-from langchain_community.chat_models import ChatOCIGenAI
-from langchain_core.messages import HumanMessage
-from langchain_core.prompts import PromptTemplate
-from langchain.schema.output_parser import StrOutputParser
-from langchain.schema.runnable import RunnablePassthrough
-from langchain_community.llms.oci_generative_ai import OCIGenAI
-from langchain.chains.llm import LLMChain
-
-
-from langchain_core.documents import Document
-from langchain_community.embeddings import OCIGenAIEmbeddings
 import pandas as pd
 from openpyxl import load_workbook
 from openpyxl.drawing.image import Image as XLImage
 from PIL import Image
 import io
-import os
 import base64
-from tabulate import tabulate
 from dotenv import load_dotenv, find_dotenv
-from moviepy import  VideoFileClip, ColorClip, CompositeVideoClip, concatenate_videoclips
-from moviepy.audio.io.AudioFileClip import AudioFileClip
-import cv2
 
-from utils.utils import dir_check, get_embedding, summarize_image_to_text, summarize_text, upload_image_to_oci
+from utils.utils import get_embedding, summarize_image_to_text, summarize_text, upload_image_to_oci
 
 _ = load_dotenv(find_dotenv())
 oracledb.init_oracle_client()
@@ -52,36 +17,12 @@ UN = os.environ.get("UN")
 PW = os.environ.get("PW")
 DSN = os.environ.get("DSN")
 
-OCI_CONFIG_FILE = "~/.oci/config"
-OCI_COMPARTMENT_ID = os.getenv("OCI_COMPARTMENT_ID")
-OCI_GENAI_ENDPOINT = os.getenv("OCI_GENAI_ENDPOINT")
-OCI_OS_NAMESPACE = os.getenv("OCI_OS_NAMESPACE")
-OCI_OS_BUCKET_NAME = os.getenv("OCI_OS_BUCKET_NAME")
-OCI_OS_BUCKET_URL = os.getenv("OCI_OS_BUCKET_URL")
-MOVIE_DIRECTORY_PATH = os.getenv("MOVIE_DIRECTORY_PATH")
-SPLIT_MOVIE_DIRECTORY_PATH = os.getenv("SPLIT_MOVIE_DIRECTORY_PATH")
-
-
-config_osaka = oci.config.from_file(file_location=OCI_CONFIG_FILE, profile_name="OSAKA")
-config = oci.config.from_file(file_location=OCI_CONFIG_FILE, profile_name="DEFAULT")
-generative_ai_inference_client = GenerativeAiInferenceClient(config_osaka, sevice_endpoint=OCI_GENAI_ENDPOINT)
-model_id="ocid1.generativeaimodel.oc1.ap-osaka-1.amaaaaaask7dceyayobenuynf4om42tyrr7scxwijpzwfajc6i6w5wjcmjbq"
-
-preamble = """
-## 回答のスタイル
-日本語で回答してください。
-質問に対してできる限り詳細な回答をしてください。
-"""
-input_text = "Oracleのクラウドについて教えてください"
-
-
 def extract_tables_and_images(file_path, output_dir="images"):
   os.makedirs(output_dir, exist_ok=True)
   wb = load_workbook(file_path)
   e_wb = pd.ExcelFile(file_path)
   result = []
 
-  # check_sheets = {"Segmental info & Opex", "Corporate_Overview"}
   for sheet_name in wb.sheetnames:
     # if not sheet_name in check_sheets:
     #   continue
@@ -109,20 +50,15 @@ def extract_tables_and_images(file_path, output_dir="images"):
         img_path = os.path.join(output_dir, img_name)
         img.save(img_path)
         print(f"Image Path: {img_path}")
-        
-        with open(img_path, "rb") as f:
-          base64_str = base64.b64encode(f.read()).decode("utf-8")
-          data_url = f"data:image/png;base64,{base64_str}"
 
         result.append({
           "type": "image",
           "sheet": sheet_name,
           "image_path": img_path,
-          "image_url": data_url
         })
   return result
 
-def save_file_info(file_path):
+def save_file_info(file_path: str):
   try:
     with oracledb.connect(user=UN, password=PW, dsn=DSN) as conn:
       cur = conn.cursor()
@@ -141,7 +77,7 @@ def save_file_info(file_path):
     print(f"Oracle error message: {error.message}")
     return None
 
-def save_docs_content(file_id, markdown, summary, embedding):
+def save_docs_content(file_id, markdown: str, summary: str, embedding: list):
   sql = """
           INSERT INTO docs_contents (file_id, summary, embedding, markdown)
           VALUES (:file_id, :summary, :embedding, :markdown)
@@ -167,10 +103,10 @@ def save_docs_content(file_id, markdown, summary, embedding):
     print(f"Error:save_docs_content: {e}")
     return None
 
-def save_image_content(file_id, image_path, image_url, summary, embedding):
+def save_image_content(file_id: str, image_path: str, summary: str, embedding: list):
   sql = """
-        INSERT INTO image_contents (file_id, image_path, image_url, summary, embedding, image_blob)
-        VALUES (:file_id, :image_path, :image_url, :summary, :embedding, empty_blob())
+        INSERT INTO image_contents (file_id, image_path, summary, embedding, image_blob)
+        VALUES (:file_id, :image_path, :summary, :embedding, empty_blob())
         returning image_blob into :blobdata
       """
   
@@ -181,7 +117,6 @@ def save_image_content(file_id, image_path, image_url, summary, embedding):
         params = {
           'file_id': file_id,
           'image_path': image_path,
-          'image_url': image_url,
           'summary': summary,
           'embedding': str(embedding),
           'blobdata': blobdata
@@ -210,22 +145,21 @@ def save_image_content(file_id, image_path, image_url, summary, embedding):
     print(f"Error:save_image_content: {e}")
     return None
 
-def summarize_to_db_and_upload_image(image_path, object_name=None):
+
+def summarize_to_db_and_upload_image(image_path):
   with open(image_path, "rb") as img_file:
     image_lob = img_file.read()
     print(f"type: {type(image_lob)}")
   
-  uploaded_url = upload_image_to_oci(image_path, object_name)
-  print(f"Uploaded Image URL: {uploaded_url}")
-  
   print(f"image_path: {image_path}")
-  image_summary = summarize_image_to_text(image_path, uploaded_url)
+  image_summary = summarize_image_to_text(image_path)
   print(f"Image Summary: {image_summary}")
   image_embedding = get_embedding(image_summary)
   
   image_id = save_file_info(image_path)
-  save_image_content(image_id, image_path, uploaded_url, image_summary, image_embedding)
-  return {"summary": image_summary, "uploaded_url": uploaded_url}
+  save_image_content(image_id, image_path, image_summary, image_embedding)
+  return {"summary": image_summary}
+
 
 def process_excel_with_images(file_path):
   file_id = save_file_info(file_path)
@@ -241,158 +175,11 @@ def process_excel_with_images(file_path):
     elif content["type"] == "image":
       print(f"content_type:image: {content["type"]}")
       res = summarize_to_db_and_upload_image(
-        image_path=content["image_path"], 
-        object_name=os.path.basename(content["image_path"])
+        image_path=content["image_path"]
         )
-
-
-# Movies
-def resize_image(input_path, output_path, max_width=2560, max_height=1440):
-  image = cv2.imread(input_path)
-  if image is None:
-    print(f"Failed to load image: {input_path}")
-    return
-
-  height, width = image.shape[:2]
-
-  if width <= max_width and height <= max_height:
-      print(f"Image is already within the size limits: {input_path}")
-      cv2.imwrite(output_path, image)
-      return
-
-  aspect_ratio = width / height
-  if width / max_width > height / max_height:
-    new_width = max_width
-    new_height = int(max_width / aspect_ratio)
-  else:
-    new_height = max_height
-    new_width = int(max_height * aspect_ratio)
-
-  resized_image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
-
-  cv2.imwrite(output_path, resized_image)
-  print(f"Resized image saved to: {output_path}")
-
-def summarize_to_db_and_upload_movie(image_path, object_name=None):
-  with open(image_path, "rb") as img_file:
-    image_lob = img_file.read()
-    print(f"type: {type(image_lob)}")
-  
-  uploaded_url = upload_image_to_oci(image_path, object_name)
-  print(f"Uploaded Image URL: {uploaded_url}")
-  
-  image_summary = summarize_image_to_text(image_path, uploaded_url)
-  print(f"Image Summary: {image_summary}")
-  if image_summary is not None:
-    image_embedding = get_embedding(image_summary)
-  
-  image_id = save_file_info(image_path)
-  save_image_content(image_id, image_path, uploaded_url, image_summary, image_embedding)
-  return {"summary": image_summary, "uploaded_url": uploaded_url}
-
-
-def save_frames_at_intervals(
-  video_path: str, 
-  interval_sec: int, 
-  output_dir: str, 
-  base_filename: str
-  ) -> None:
-  try:
-    video = VideoFileClip(video_path)
-    duration_sec = video.duration
-    current_sec = 0
-    frame_idx = 0
-    digit = len(str(int(duration_sec // interval_sec) + 1))
-
-    while current_sec < duration_sec:
-      frame = video.get_frame(current_sec)
-      output_filename = '{}_{}_{:.2f}.png'.format(base_filename, str(frame_idx).zfill(digit), current_sec)
-      output_path = os.path.join(output_dir, output_filename)
-
-      frame_image = Image.fromarray(frame)
-      frame_image.save(output_path)
-      print(f"Saved frame at {current_sec} sec to {output_path}")
-    
-      current_sec += interval_sec
-      frame_idx += 1
-
-    video.close()
-
-  except Exception as e:
-    print(f"Error processing video {video_path}: {e}")
-
-def split_movie(movie_file: str) -> None:
-  try:
-    if movie_file.endswith(".mp4"):
-      movie_path = os.path.join(MOVIE_DIRECTORY_PATH, movie_file)
-      base_filename = movie_file.replace(".mp4", "")
-      save_frames_at_intervals(movie_path, 10, SPLIT_MOVIE_DIRECTORY_PATH, base_filename)
-      print(f"split {MOVIE_DIRECTORY_PATH}/{movie_file}")
-  except Exception as e:
-    print(f"Error splitting movie {movie_file}: {e}")
-  
-def split_movies() -> None:
-    dir_check(SPLIT_MOVIE_DIRECTORY_PATH, ".png")
-    movie_files = os.listdir(MOVIE_DIRECTORY_PATH)
-    print(movie_files)
-    try:
-      for movie_file in movie_files:
-        split_movie(movie_file)
-    except Exception as e:
-      print("Error split movies", e)
-
-def encode_image(image_path: str):
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode("utf-8")
-
-def process_movies() -> None:
-  
-  split_movies()
-
-  for movie_file in os.listdir(SPLIT_MOVIE_DIRECTORY_PATH):
-    if movie_file.endswith(".png"):
-      image_path = os.path.join(SPLIT_MOVIE_DIRECTORY_PATH, movie_file)
-      print(f"image_path: {image_path}")
-      resize_image(
-        input_path=os.path.join(image_path),
-        output_path=os.path.join(image_path),
-        max_width=2560,
-        max_height=1440
-      )
-      
-      summarize_to_db_and_upload_movie(image_path, object_name=os.path.basename(image_path))
-
-def excel_markdown(file_path):
-  wb = load_workbook(file_path)
-  e_wb = pd.ExcelFile(file_path)
-  result = []
-
-  for sheet_name in wb.sheetnames:
-    # if not sheet_name in check_sheets:
-    #   continue
-    ws = wb[sheet_name]
-    print(f"worksheet: {ws}")
-    df = pd.read_excel(e_wb, sheet_name=sheet_name)
-    markdown = df.to_markdown(index=False)
-    print(f"Markdown Table:\n{markdown}")
-    result.append({
-      "type": "table",
-      "sheet": sheet_name,
-      "markdown": markdown,
-      "raw": df
-    })
-  return result
-
-def process_excel(file_path: str) -> None:
-  file_id = save_file_info(file_path)
-  contents = excel_markdown(file_path)
-  for content in contents:
-    summary = summarize_text(content["markdown"])
-    embedding = get_embedding(summary)
-    save_docs_content(file_id, content["markdown"], summary, embedding)
 
 
 if __name__ == "__main__":
   process_excel_with_images("./data/fy25q3-supplemental.xlsx")
-  summarize_to_db_and_upload_image(image_path="./images/net_income.png", object_name="net_income.png")
-  summarize_to_db_and_upload_image(image_path="./images/revenue.png", object_name="revenue.png")
+  summarize_to_db_and_upload_image(image_path="./images/net_income.png")
+  summarize_to_db_and_upload_image(image_path="./images/revenue.png")
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,50 +1,15 @@
 import base64
-import datetime
-import json
 import os
-import shutil
 from dotenv import find_dotenv, load_dotenv
-import oci
-from oci.generative_ai_inference import GenerativeAiInferenceClient
-from oci.generative_ai_inference.models import (
-    EmbedTextDetails,
-    OnDemandServingMode,
-)
-from oci.object_storage import ObjectStorageClient
-from oci.ai_speech import AIServiceSpeechClient
-from oci.ai_speech.models import (
-  CreateTranscriptionJobDetails,
-  ObjectListInlineInputLocation,
-  ObjectLocation,
-  OutputLocation,
-  TranscriptionModelDetails,
-  TranscriptionJobSummary,
-
-)
 from langchain_community.embeddings import OCIGenAIEmbeddings
 from langchain_core.prompts import PromptTemplate
 from langchain.schema.runnable import RunnablePassthrough
 from langchain_community.chat_models import ChatOCIGenAI
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.messages import HumanMessage, SystemMessage
-from langchain.prompts.chat import ChatPromptTemplate
 
 
 _ = load_dotenv(find_dotenv())
-OCI_CONFIG_FILE = "~/.oci/config"
-OCI_GENAI_ENDPOINT = os.getenv("OCI_GENAI_ENDPOINT")
 OCI_COMPARTMENT_ID = os.getenv("OCI_COMPARTMENT_ID")
-OCI_OS_NAMESPACE = os.getenv("OCI_OS_NAMESPACE")
-OCI_OS_BUCKET_NAME = os.getenv("OCI_OS_BUCKET_NAME")
-OCI_OS_BUCKET_URL = os.getenv("OCI_OS_BUCKET_URL")
-
-config = oci.config.from_file(file_location=OCI_CONFIG_FILE, profile_name="DEFAULT")
-object_storage = ObjectStorageClient(config)
-speech_client = AIServiceSpeechClient(config)
-generative_ai_inference_client = GenerativeAiInferenceClient(
-    config=config,
-    service_endpoint="https://inference.generativeai.us-chicago-1.oci.oraclecloud.com"
-    )   
 
 def get_embedding(text: str) -> list:
   embeddings = OCIGenAIEmbeddings(
@@ -53,6 +18,7 @@ def get_embedding(text: str) -> list:
     compartment_id=OCI_COMPARTMENT_ID,
   )
   return embeddings.embed_query(text)
+
 
 def summarize_text(text: str) -> str:
   prompt = PromptTemplate(
@@ -65,7 +31,7 @@ def summarize_text(text: str) -> str:
     compartment_id=OCI_COMPARTMENT_ID,
     model_kwargs={"temperature": 0.7, "max_tokens": 500},
     )
-  # chain = LLMChain(llm=llm, prompt=prompt)
+
   chain = ({"text": RunnablePassthrough() } 
            | prompt 
            | llm 
@@ -75,7 +41,7 @@ def summarize_text(text: str) -> str:
   return result
 
 
-def summarize_image_to_text(image_path: str, uploaded_url: str = None) -> str:
+def summarize_image_to_text(image_path: str) -> str:
   with open(image_path, "rb") as img_file:
     image_data = base64.b64encode(img_file.read()).decode("utf-8")
 
@@ -95,7 +61,7 @@ def summarize_image_to_text(image_path: str, uploaded_url: str = None) -> str:
     ],
   }
   llm = ChatOCIGenAI(
-    model_id="meta.llama-3.2-90b-vision-instruct",
+    model_id="meta.llama-4-scout-17b-16e-instruct",
     service_endpoint="https://inference.generativeai.us-chicago-1.oci.oraclecloud.com",
     compartment_id=OCI_COMPARTMENT_ID,
     )
@@ -107,211 +73,6 @@ def summarize_image_to_text(image_path: str, uploaded_url: str = None) -> str:
       print(f"Error in image summarization: {e}")
       return None
 
-def chat_with_image(image_path: str, prompt: str, system_prompt: str = None) -> str:
-  with open("/home/opc/multimodal_oci_genai/" + image_path, "rb") as img_file:
-    image_data = base64.b64encode(img_file.read()).decode("utf-8")
-  
-  prompt_with_image = [
-    SystemMessage(
-        content=system_prompt
-    ),
-    HumanMessage(
-        content=[
-            {"type": "text", "text": prompt},
-            {
-              "type": "image_url",
-              "image_url": {
-                "url": "data:image/png;base64,"+image_data,
-            }
-        },
-        ]
-      )
-  ]
-
-  llm = ChatOCIGenAI(
-      model_id="meta.llama-3.2-90b-vision-instruct",
-      service_endpoint="https://inference.generativeai.us-chicago-1.oci.oraclecloud.com",
-      compartment_id=OCI_COMPARTMENT_ID,
-      )
-  result = llm.invoke(prompt_with_image)
-  print(f"Result: {result}") 
-  return result.content
-  
-
-def chat_cohere(prompt: str, system_prompt: str = "") -> str:
-  
-  chat_prompt = ChatPromptTemplate.from_messages([
-    ("system", "{system_prompt}"),
-    ("human", "{input}"),
-  ])
-
-
-  llm = ChatOCIGenAI(
-      model_id="cohere.command-r-08-2024",
-      service_endpoint="https://inference.generativeai.us-chicago-1.oci.oraclecloud.com",
-      compartment_id=OCI_COMPARTMENT_ID,
-      )
-  # chain = chat_prompt | llm | StrOutputParser()
-  # result = chain.invoke({"input": prompt, "system_prompt": system_prompt})
-  result = llm.invoke(chat_prompt)
-  return result.content
-
-def upload_image_to_oci(image_path, object_name=None):
-  if not object_name:
-    object_name = os.path.basename(image_path)
-
-  with open(image_path, "rb") as f:
-    data = f.read()
-
-  object_storage.put_object(
-    namespace_name=OCI_OS_NAMESPACE,
-    bucket_name=OCI_OS_BUCKET_NAME,
-    object_name=object_name,
-    put_object_body=data
-  )
-  object_url = OCI_OS_BUCKET_URL + object_name
-  return object_url
-
-def download_image_from_oci(object_name: str, destination_path: str) -> str:
-    response = object_storage.get_object(
-        namespace_name=OCI_OS_NAMESPACE,
-        bucket_name=OCI_OS_BUCKET_NAME,
-        object_name=object_name
-    )
-
-    with open(destination_path, "wb") as out_file:
-      for chunk in response.data.raw.stream(1024 * 1024, decode_content=False):
-        out_file.write(chunk)
-
-    return destination_path
-
-def delete_files(directory: str, file_type: str) -> None:
-    """ Delete files """
-    files = os.listdir(directory)
-    try:
-        for file in files:
-            if file.endswith(file_type):
-                os.remove(os.path.join(directory, file))
-    except FileNotFoundError:
-        print("no target files")
-    except Exception as e:
-        print("Error delete files", e)
-
-def dir_check(directory: str, file_type: str) -> None:
-    """ Check directory """
-    if os.path.exists(directory):
-        try:
-            delete_files(directory, file_type)
-            print(f"success delete files in {directory}")
-        except FileNotFoundError:
-            print("no target files")
-        except Exception as e:
-            print("Error delete files", e)
-    else:
-        try:
-            os.makedirs(directory, exist_ok=True)
-        except Exception as e:
-            print("Error make dirctory ", e)
-
-def delete_dir(directory: str) -> None:
-    """ Delete directory """
-    if os.path.exists(directory):
-        try:
-            shutil.rmtree(directory)
-            print("success delete directory")
-        except Exception as e:
-            print("Error delete directory", e)
-            
-            
-## Whisper
-def audio_to_text(uploaded_file_name: str):
-  create_transcription_job_details = CreateTranscriptionJobDetails(
-      compartment_id=OCI_COMPARTMENT_ID,
-      display_name="PythonSDKSampleTranscriptionJob",
-      description="Transcription job created by Python SDK",
-      input_location=ObjectListInlineInputLocation(
-          location_type="OBJECT_LIST_INLINE_INPUT_LOCATION",
-          object_locations=[ObjectLocation(
-              namespace_name=OCI_OS_NAMESPACE,
-              bucket_name=OCI_OS_BUCKET_NAME,
-              object_names=[uploaded_file_name])]),
-      output_location=OutputLocation(
-          namespace_name=OCI_OS_NAMESPACE, bucket_name=OCI_OS_BUCKET_NAME),
-      model_details=TranscriptionModelDetails(
-          model_type="WHISPER_MEDIUM", language_code="ja")
-  )   
-  response = speech_client.create_transcription_job(
-      create_transcription_job_details)
-  job_id = response.data.id
-
-
-  job_finished = False
-  while not job_finished:
-    job_details = speech_client.get_transcription_job(job_id).data
-    job_status = job_details.lifecycle_state
-    if job_status in ["SUCCEEDED"]:
-      job_finished = True
-      output_location = job_details.output_location
-      bucket_name = output_location.bucket_name
-      namespace_name = output_location.namespace_name
-      prefix = output_location.prefix
-      object_storage_client = ObjectStorageClient(config)
-      list_objects_response = object_storage_client.list_objects(
-          namespace_name=namespace_name,
-          bucket_name=bucket_name,
-          prefix=prefix,
-          fields="name"
-      )
-      file_name = list_objects_response.data.objects[0].name
-      get_object_response = object_storage_client.get_object(
-          namespace_name=namespace_name,
-          bucket_name=bucket_name,
-          object_name=file_name
-      )
-      file_content = get_object_response.data.text
-      transcriptions_json = json.loads(file_content)
-      print(f"Trascription result: {transcriptions_json['transcriptions']}")
-      return transcriptions_json['transcriptions']
-          
-def embedding_image(image_path: str):
-  with open("/home/opc/multimodal_oci_genai/" + image_path, "rb") as img_file:
-    image_data = base64.b64encode(img_file.read()).decode("utf-8")
-    data_uri = f"data:image/png;base64,{image_data}"
-
-  print(f"data_uri: {data_uri}")
-  model_id = "cohere.embed-multilingual-image-v3.0"
-  embed_image_detail = EmbedTextDetails()
-  embed_image_detail.serving_mode = OnDemandServingMode(model_id=model_id)
-  embed_image_detail.compartment_id = OCI_COMPARTMENT_ID
-
-  embed_image_detail.inputs = [data_uri]
-  embed_image_detail.input_type = "IMAGE"
-  embedding = generative_ai_inference_client.embed_text(embed_image_detail)
-  print(f"Embedding result: {embedding.data.embeddings}")
-  return embedding.data.embeddings
-  
-
 
 if __name__ == "__main__":
-  # response = get_embedding(
-  #     text="This is a test text for embedding."
-  # )
-  # print(response)
-  
-  # response = summarize_image_to_text(
-  #     image_path="./images/製品概要_1_7.png",
-  #     uploaded_url=None
-  # )
-  # print(response)
-  
-  # res = chat_with_image(
-  #     image_path="images/net_income.png",
-  #     prompt="Tell me the operating profit for FY24.",
-  #     system_prompt="You are a helpful assistant."
-  # )
-  # print(res)
-  
-  res = embedding_image(
-      image_path="images/製品概要_1_7.png"
-  )
-  print(res)
+  print("test")
